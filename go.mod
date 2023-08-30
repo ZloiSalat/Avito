138356924fa3625dcd87c468f1627f0de030e076
ZloiSalat <darbinyanvm@yandex.ru>
2023-08-30 23:17:50 +0300

    Created

--- go.mod
+++ go.mod
@@ -7,16 +7,24 @@ require (
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/georgysavva/scany v1.2.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.9.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.15.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.8.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.6 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.6.2 // indirect
 	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.10.1 // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jackc/puddle v1.1.3 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
@@ -34,6 +42,7 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,223 @@
 package main
 
 import (
-	"net/http"
-
+	"context"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
+const DBUrl = "postgres://avito_user:avito_password@localhost:5434/avito_db"
+
+var db *pgx.Conn
+
+type Segment struct {
+	ID      int    `json:"id"`
+	Segment string `json:"slug"`
+}
+
+func CreateSegment(c *gin.Context) {
+	var segment Segment
+	c.BindJSON(&segment)
+
+	_, err := db.Exec(context.Background(), "INSERT INTO segments (segment_name) VALUES ($1)", segment.Segment)
+	if err != nil {
+		panic(err)
+	}
+
+	c.JSON(201, gin.H{"message": "Segment created successfully!"})
+}
+
+func DeleteSegment(c *gin.Context) {
+	slug := c.Param("slug")
+	_, err := db.Exec(context.Background(), "DELETE FROM segments WHERE segment_name=$1", slug)
+	if err != nil {
+		panic(err)
+	}
+
+	c.JSON(200, gin.H{"message": "Segment deleted successfully!"})
+}
+
+func getSegmentIDByUserID(UserID string) ([]int, error) {
+	var segmentId []int
+
+	query := "SELECT segment_id FROM user_segments WHERE user_id = $1;"
+	intUserID, _ := strconv.Atoi(UserID)
+	//result, err := db.Exec(context.Background(), `Delete from t where col=$1`, "val")
+	rows, err := db.Query(context.Background(), query, intUserID)
+	if err != nil {
+		log.Fatal("Error querying database:", err)
+	}
+	log.Println(&rows)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var segment int
+		err := rows.Scan(&segment)
+		log.Println(segment)
+		if err != nil {
+			log.Fatal("Error scanning rows:", err)
+		}
+		segmentId = append(segmentId, segment)
+	}
+	log.Println(segmentId)
+	if err := rows.Err(); err != nil {
+		log.Fatal("Error iterating rows:", err)
+	}
+
+	return segmentId, err
+
+}
+
+func AddUserToSegment(c *gin.Context) {
+
+	var request struct {
+		UserID         int      `json:"user_id"`
+		AddSegments    []string `json:"add_segments"`
+		RemoveSegments []string `json:"remove_segments"`
+	}
+
+	c.BindJSON(&request)
+
+	if len(request.AddSegments) == 0 && len(request.RemoveSegments) == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "No actions with segments"})
+		return
+	}
+
+	if len(request.AddSegments) != 0 {
+		addQuery := "SELECT s.id FROM segments s left join user_segments us on s.id = us.segment_id and user_id = $1 where us.segment_id is null and s.segment_name = any($2);"
+		rows, err := db.Query(context.Background(), addQuery, request.UserID, request.AddSegments)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		defer rows.Close()
+
+		var result string
+		for rows.Next() {
+			var answer int
+			err := rows.Scan(&answer)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			result += fmt.Sprintf("(%d, %d), ", request.UserID, answer)
+
+		}
+		if err := rows.Err(); err != nil {
+			log.Fatal(err)
+		}
+		result = strings.TrimSuffix(result, ", ")
+		log.Println(result)
+
+		hueusQuery := fmt.Sprintf("insert into user_segments (user_id, segment_id) values %s;", result)
+
+		_, err = db.Exec(context.Background(), hueusQuery)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+	}
+
+	if len(request.RemoveSegments) != 0 {
+		getDeleteSegment := "SELECT s.id FROM segments s where s.segment_name = any($1);"
+		deleteRows, err := db.Query(context.Background(), getDeleteSegment, request.RemoveSegments)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+
+		defer deleteRows.Close()
+
+		var idSlice []int
+
+		for deleteRows.Next() {
+			var deleteAnswer int
+			err := deleteRows.Scan(&deleteAnswer)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			idSlice = append(idSlice, deleteAnswer)
+		}
+		if err := deleteRows.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		deleteQuery := "DELETE FROM user_segments WHERE segment_id = any($1) and user_id = $2;"
+
+		_, err = db.Exec(context.Background(), deleteQuery, idSlice, request.UserID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Ok!"})
+}
+
+func GetActiveSegments(c *gin.Context) {
+	userID := c.Param("id")
+
+	//query := "select segment_id from user_segments where user_id = $1;"
+	query := `SELECT s.segment_name
+			  FROM segments s
+			  INNER JOIN user_segments us ON s.id = us.segment_id
+			  WHERE us.user_id = $1;`
+
+	rows, err := db.Query(context.Background(), query, userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var segments []string
+	for rows.Next() {
+		var segment string
+		err := rows.Scan(&segment)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		segments = append(segments, segment)
+	}
+	c.JSON(http.StatusOK, gin.H{"segments": segments})
+}
+
+func NewClient() {
+
+	conn, err := pgx.Connect(context.Background(), DBUrl)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	//defer conn.Close(context.Background())
+	db = conn
+	log.Println("Connected successfully!")
+
+}
+
 func main() {
+	NewClient()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Привет, я Gin",
 		})
 	})
-	err := r.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	if err != nil {
-		return
-	}
+
+	r.POST("/segment", CreateSegment)
+	r.POST("/user2segment", AddUserToSegment)
+	r.GET("/segments/:id", GetActiveSegments)
+	r.DELETE("/segment/:slug", DeleteSegment)
+	r.Run(":8000")
+
 }
